30074c5fc3f7aed008677700096593397022be9a
Tanveer Bashir <tan.m.bashir@gmail.com>
2018-03-13 19:15:56 -0400

    Add my_friends view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => 'user/registrations' }
   root  'welcome#index'
-
   get 'my_portfolio', to: 'users#my_portfolio'
-
   get 'search_stocks', to: 'stocks#search'
-
   resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: 'users#my_friends'
+  resources :users, only: [:show]
+  resources :friendships
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
